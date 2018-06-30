9eb4f7c0b4bd03f4aa1f7d775230840ba17842f3
Giovanni Flores <gdr943@gmail.com>
2018-06-30 13:57:24 -0700

    Jobs displayed

--- TheCSLifeSite/jobs/views.py
+++ TheCSLifeSite/jobs/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import Job
 # Create your views here.
+
+
+def home(request):
+    context = Job.objects.all()
+    return render(request, 'jobs/home.html', {'jobs':context})
