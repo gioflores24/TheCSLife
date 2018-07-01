b1c31492d18ddedbb94d1372827a978c0fdcd9e3
Giovanni Flores <gdr943@gmail.com>
2018-06-30 19:05:31 -0700

    Added blogs to site

--- TheCSLifeSite/Blog/views.py
+++ TheCSLifeSite/Blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import Blog
 # Create your views here.
+
+
+def blogs(request):
+    context = Blog.objects.all()
+    return render(request, 'blog/blogs.html', {'blogs': context})
