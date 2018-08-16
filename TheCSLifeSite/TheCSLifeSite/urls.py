f3911cbfbbff58df6f9c94d32073929ba12135bf
Giovanni Flores <gdr943@gmail.com>
2018-08-15 22:41:44 -0700

    Created resume app

--- TheCSLifeSite/TheCSLifeSite/urls.py
+++ TheCSLifeSite/TheCSLifeSite/urls.py
@@ -22,5 +22,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('jobs.urls')),
-    path('blog/', include('blog.urls'))
+    path('blog/', include('blog.urls')),
+    path('resume/',include('resume.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ TheCSLifeSite/resume/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Resume(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    img = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.name
+
